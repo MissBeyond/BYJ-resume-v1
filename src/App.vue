31e23f96faed0a805cdda8f2e96b87f56b1e9223
Beyond <2546776177@qq.com>
2018-06-25 17:19:25 +0800

    first add

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div id="App">
+  <resume></resume>
+</div>
+  
+</template>
+
+<script>
+import Resume from 'components/Resume'
+export default {
+  name: 'App',
+  components: {
+    Resume
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  
+</style>
---
+++ src/components/Resume.vue
@@ -0,0 +1,366 @@
+<template>
+    <div class="resume-warpper">
+        <div class="resume-left">
+            <div class="avatar">
+                <img src="../assets/byj.jpeg" width="90" height="90">
+            </div>
+            <div class="job">
+                <span class="name">岗位：WEB前端</span>
+            </div>
+            <div class="info-wrapper">
+                <div class="info">
+                    <div class="icon">
+                        <i class="iconfont icon-jibenxinxi2"></i>
+                    </div>
+                    <div class="title-wrapper">
+                        <span class="title">基本信息</span>
+                    </div>
+                </div>
+                <div class="des">
+                    <div class="title">
+                        <i class="iconfont icon-icon-person"></i>
+                        <p class="name">姓&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp名：毕扬君</p>
+                    </div>
+                    <div class="title">
+                        <i class="iconfont icon-icon-person"></i>
+                        <p class="name">性&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp别：女</p>
+                    </div>
+                    <div class="title">
+                        <i class="iconfont icon-biyejiaoyuhuodong"></i>
+                        <p class="name">毕业学校：四川师范大学</p>
+                    </div>
+                    <div class="title">
+                        <i class="iconfont icon-zhuanyezhishijineng"></i>
+                        <p class="name">专&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp业：环境工程</p>
+                    </div>
+                </div>
+            </div>
+            <div class="info-wrapper">
+                <div class="info">
+                    <div class="icon">
+                        <i class="iconfont icon-wo"></i>
+                    </div>
+                    <div class="title-wrapper">
+                        <span class="title">联系方式</span>
+                    </div>
+                </div>
+                <div class="des">
+                    <div class="title">
+                        <i class="iconfont icon-dianhua1"></i>
+                        <p class="name">电&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp话：13981739826</p>
+                    </div>
+                    <div class="title">
+                        <i class="iconfont icon-tubiao209"></i>
+                        <p class="name">邮&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp箱：2546776177@qq.com</p>
+                    </div>
+                    <div class="title">
+                        <i class="iconfont icon-shouye"></i>
+                        <p class="name">住&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp址：深圳南山区</p>
+                    </div>
+                </div>
+            </div>
+            <div class="info-wrapper">
+                <div class="info">
+                    <div class="icon">
+                        <i class="iconfont icon-gongju1"></i>
+                    </div>
+                    <div class="title-wrapper">
+                        <span class="title">专业技能</span>
+                    </div>
+                </div>
+                <ul class="skill">
+                    <li class="detail">
+                        <p class="title">HTML</p>
+                        <span class="progress"><span class="highlight"></span></span>
+                    </li>
+                    <li class="detail">
+                        <p class="title">CSS</p>
+                        <span class="progress"><span class="highlight css"></span></span>
+                    </li>
+                    <li class="detail">
+                        <p class="title">JavaScipt</p>
+                        <span class="progress"><span class="highlight js"></span></span>
+                    </li>
+                    <li class="detail">
+                        <p class="title">Vue</p>
+                        <span class="progress"><span class="highlight vue"></span></span>
+                    </li>
+                    <li class="detail">
+                        <p class="title">PhotoShop</p>
+                        <span class="progress"><span class="highlight ps"></span></span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="resume-right">
+            <div class="work">
+                <div class="title">
+                    <div class="icon">
+                        <i class="iconfont icon-gongju1"></i>
+                    </div>
+                    <span class="name">工作经历</span>
+                    <span class="name-english">WORK&nbsp&nbspEXPERIENCE</span>
+                    </div>
+                <div class="desc">
+                    <div class="job">
+                        <i class="iconfont icon-ic_pending"></i>
+                        <span class="time">2017/06~2017/08</span>
+                        <span class="company">科全经贸有限公司</span>
+                        <span class="jobname">工作职位：电商美工</span>
+                        <p class="text">工作内容：负责公司淘宝、天猫店铺的装修、宠物产品的拍摄、图片后期处理，详情页、主图、海报设计</p>
+                    </div>
+                    <div class="job">
+                        <i class="iconfont icon-ic_pending"></i>
+                        <span class="time">2017/09~2018/05</span>
+                        <span class="company abs">深圳市中天兴衍科技有限公司</span>
+                        <span class="jobname">工作职位：设计/前端</span>
+                        <p class="text">工作内容：主要负责设计各类活动的海报（节假日、早起卡、晚安图等），完成活动页面设计及前端静态页实现</p>
+                    </div>
+                </div>
+            </div>
+            <div class="work">
+                <div class="title">
+                    <div class="icon">
+                        <i class="iconfont icon-xiangmu"></i>
+                    </div>
+                    <span class="name">项目经历</span>
+                    <span class="name-english">PROJECTS&nbsp&nbspEXPERIENCE</span>
+                    </div>
+                <div class="desc">
+                    <div class="job">
+                        <i class="iconfont icon-ic_pending"></i>
+                        <span class="time">早起打卡页面的设计加前端静态页的实现</span>
+                        <p class="text">技术：HTML/CSS</p>
+                    </div>
+                    <div class="job">
+                        <i class="iconfont icon-ic_pending"></i>
+                        <span class="time">高仿饿了么外卖WebApp</span>
+                        <p class="text">技术栈：MVVM框架（Vue.js 2.0）、状态管理（Vuex）、前端路由（Vue-router）、服务端通讯（axios）、滚动插件（better-scroll）、构建工具（webpack）、CSS 预处理（stylus）</p>
+                    </div>
+                    <div class="job">
+                        <i class="iconfont icon-ic_pending"></i>
+                        <span class="time">Vue.js 实战去哪儿网</span>
+                        <p class="text">技术栈：Vue 全家桶及其他相关技术</p>
+                    </div>
+                    <div class="job">
+                        <i class="iconfont icon-ic_pending"></i>
+                        <span class="time">快餐店的POS系统</span>
+                        <p class="text">系统使用的技术栈为：Vue+Webpack+Element+Axios+vueRouter</p>
+                    </div>
+                </div>
+            </div>
+            <div class="work">
+                <div class="title">
+                    <div class="icon">
+                        <i class="iconfont icon-aixin"></i>
+                    </div>
+                    <span class="name">自我评价</span>
+                    <span class="name-english">SELF&nbsp&nbspEVALUATION</span>
+                    </div>
+                <div class="desc">
+                    <div class="job">
+                        <p class="text extra">1.良好的沟通能力，性格开朗，乐于分享，喜欢有活力的团队</p>
+                        <p class="text extra">2.自学能力强，善于思考，善于与他人沟通，有团队合作精神</p>
+                        <p class="text extra">3.自己也有不足之处，比如：社会经验等欠缺，不过这些可以通过自身的努力而达到的</p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Resume'
+}
+</script>
+<style lang="stylus" scoped>
+    .resume-warpper
+        position absolute
+        top 0
+        left 0
+        width 595px
+        height 842px
+        .resume-left
+            position relative
+            float left
+            width 40%
+            height 100%
+            background-color #2f3a40
+            .avatar
+                position relative
+                margin 30px auto 20px 
+                width 90px
+                height 90px
+                img
+                    border-radius 50%
+            .job
+                margin 0 auto
+                text-align center
+                .name
+                    display inline-block
+                    line-height 14px
+                    font-size 14px
+                    font-weight 700
+                    color #ffffff
+                    padding-bottom 6px
+                    border-bottom 2px solid #f6961e
+            .info-wrapper
+                margin-top 30px
+                .info
+                    display flex
+                    align-items center
+                    padding-left 12px
+                    font-size 0
+                    .icon
+                        display flex
+                        justify-content center
+                        align-items center
+                        z-index 99
+                        width 28px
+                        height 28px
+                        border-radius 50%
+                        border 2px solid #2f3a40
+                        background-color #f6961e
+                        .iconfont
+                            font-size 16px
+                            color #ffffff
+                    .title-wrapper
+                        display inline-block
+                        position absolute
+                        margin-left 20px
+                        z-index 80
+                        width 100px
+                        background-color #f6961e
+                        text-align center
+                        border-radius 4px
+                        .title
+                            font-size 14px
+                            font-weight 700
+                            color #ffffff
+                .des
+                    margin 10px 0 0 20px
+                    .title
+                        line-height 12px
+                        font-size 0
+                        margin-bottom 10px
+                        .iconfont
+                            display inline-block
+                            vertical-align top
+                            margin-right 6px
+                            font-size 14px
+                            color rgb(#c9cdce)
+                        .name
+                            display inline-block
+                            font-size 12px
+                            margin 0
+                            color rgb(#ffffff)
+            .info-wrapper
+                clear both
+                .skill
+                    margin 10px 0 0 20px
+                    padding-left 0
+                    .detail
+                        line-height 12px
+                        font-size 0
+                        margin-bottom 10px
+                        .title
+                            font-size 12px
+                            color #ffffff
+                            margin 0
+                        .progress
+                            display inline-block
+                            width 180px
+                            height 8px
+                            border-radius 4px
+                            background-color rgb(#d2d2d2)
+                            .highlight
+                                display inline-block
+                                width 75%
+                                height 99%
+                                border-radius 4px
+                                background-color #f6961e
+                            .css
+                                width 68%
+                            .js
+                                width 50%
+                            .vue
+                                width 60%
+                            .ps
+                                width 55%
+                
+        .resume-right
+            float right
+            width 60%
+            height 100%
+            background-color #ffffff
+            .work
+                position relative
+                margin 30px auto 20px
+                .title
+                    display flex
+                    font-size 0
+                    align-items center
+                    .icon
+                        display flex
+                        justify-content center
+                        align-items center
+                        margin-left -17px
+                        width 30px
+                        height 30px
+                        border-radius 50%
+                        border 2px solid #2f3a40
+                        background-color #fff
+                        .iconfont
+                            font-size 20px
+                            color #f6961e
+                    .name
+                        display inline-block
+                        margin-left 10px
+                        font-size 18px
+                        color #2f3a40
+                        line-height 18px
+                        font-weight 800
+                    .name-english
+                        display inline-block
+                        margin-left 10px
+                        font-size 16px
+                        color #abacaf
+            .desc
+                margin 4px 27px
+                .job
+                    margin-bottom 20px
+                    font-size 0
+                    color #2f3a40
+                    .time
+                        display inline-block
+                        margin 0 10px 0 10px
+                        font-size 14px
+                        font-weight 700
+                    .company
+                        display inline-block
+                        font-size 14px
+                        font-weight 700 
+                    .abs
+                        margin-left 26px
+                    .jobname
+                        display inline-block
+                        margin-left 26px
+                        font-size 14px
+                        font-weight 700
+                    .text
+                        display inline-block
+                        margin 0
+                        margin-top 6px
+                        margin-left 26px
+                        font-size 12px
+                    .extra
+                        margin-left 4px
+
+
+
+
+        
+</style>
+
+
